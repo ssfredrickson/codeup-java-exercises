656216af9d7662c4e64e7cb11d9791ca8a3e0d76
Stephen Fredrickson <stephen.fredrickson@gmail.com>
2023-06-13 15:06:28 -0500

    Syntax, Types and Variables Exersice

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,58 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        int myFavoriteNumber = 11;
+        String myString = "Weasel riding requires a small saddle.";
+        System.out.println("myString = " + myString);
+        double myNumber = 3.14;
+        System.out.println("myNumber = " + myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String class = "wont work"; ---- class is reserved
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+//        System.out.println("three = " + three);
+
+//        int x = 4;
+//        x = x + 5;
+//        System.out.println("x = " + x);
+
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        System.out.println("y = " + y);
+
+
+//        int x = 10;
+//        int y = 2;
+////        x = x / y;
+////        y = y - x;
+//        int number = y - x / y;
+//        System.out.println("y = " + number);
+
+        int bigNumber = Integer.MAX_VALUE;
+        long biggerNumber = Long.MAX_VALUE;
+        bigNumber++;
+        biggerNumber++;
+//        incrimenting will take the nubmer past its max
+
+
+        System.out.println("bigNumber = " + bigNumber);
+        System.out.println("biggerNumber = " + biggerNumber);
+//
+
+
     }
 }
